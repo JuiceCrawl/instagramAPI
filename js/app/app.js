a28f1bfda012d93dc3b3afe411d784a0da4f125b
Anna Garcia <annagarcia@Annas-MacBook-Pro.local>
2015-12-20 20:33:49 -0500

    clearer var names

--- js/app/app.js
+++ js/app/app.js
@@ -3,12 +3,10 @@
 	var app = angular.module('myInstagramApp',[]);
 	app.controller('InstagramController',['$scope',function($scope){
 
-		var company_ids = {
-			
-		}
-
-		$scope.companies = ["Pressed Juicery", "Juice Press","Well and Good","Juice Generation", "Class Pass", "Oh She Glows", "Athleta", "Lululemon"];
-		$scope.current_companies = "Pressed Juicery";
+	var follower_data
+	
+		$scope.company_name = ["Pressed Juicery", "Juice Press","Well and Good","Juice Generation", "Class Pass", "Oh She Glows", "Athleta", "Lululemon"];
+		$scope.current_company = "Pressed Juicery";
 
 		$scope.followers = "Test Names Go Here"
 
@@ -17,12 +15,3 @@
 	}]);
 
 })();
-
-
-$http({
-		  method: 'GET', url: 'https://api.instagram.com/v1/users/230881832/followed-by?access_token=400705.e4a7751.a9862f7be60c4fafa329aa11af0001ac'
-		}).then(function successCallback(response) {
-		    alert(success);
-		  }, function errorCallback(response) {
-		   	alert(error);
-		  });
